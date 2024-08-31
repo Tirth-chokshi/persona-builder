d4a9474b5a203b69e5b36f5ad107b4bb11a5cd59
System Administrator <root@Ronits-MacBook.local>
2024-08-31 09:32:51 +0530

    persona page created

--- src/app/home/builder/page.jsx
+++ src/app/home/builder/page.jsx
@@ -1,6 +1,8 @@
+import styles from "./builder.module.css";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import Image from "next/image";
 import {
   Sheet,
   SheetClose,
@@ -11,6 +13,16 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function Dashboard() {
   return (
     <>
@@ -43,23 +55,168 @@ export default function Dashboard() {
           </SheetFooter>
         </SheetContent>
         {/* ///////////////////////////////////// */}
-      <div className="flex flex-col w-full h-full items-center justify-start">
-      <h1 className="max-w-[900px] w-full text-3xl font-semibold m-[30px]">
-        Create Persona
-      </h1>
-        <div className="flex w-full h-auto items-center justify-center">
-          <Input
-            className="max-w-[600px] w-[90%] h-[40px]"
-            type="email"
-            placeholder="Search company or individual..."
-          />
-          <Button className="ml-[10px] h-[40px] w-[100px]" variant="outline">Search</Button>
-          <SheetTrigger asChild>
-            <Button className="ml-[10px] h-[40px] w-[100px]" variant="secondary">Filter</Button>
-          </SheetTrigger>
+        <div className="flex flex-col w-full h-full items-center justify-start">
+          <h1 className="max-w-[900px] w-full text-3xl font-semibold m-[30px]">
+            Create Persona
+          </h1>
+          <div className="flex w-full h-auto items-center justify-center">
+            <Input
+              className="max-w-[600px] w-[90%] h-[40px]"
+              type="email"
+              placeholder="Search company or individual..."
+            />
+            <Button className="ml-[10px] h-[40px] w-[100px]" variant="outline">
+              Search
+            </Button>
+            <SheetTrigger asChild>
+              <Button
+                className="ml-[10px] h-[40px] w-[100px]"
+                variant="secondary"
+              >
+                Filter
+              </Button>
+            </SheetTrigger>
+          </div>
+          <div className={styles.builder_cards_containder}>
+            <Card className={styles.builder_cards}>
+              <Image
+                src="/images/techr_logo.png"
+                alt="Image"
+                width="400"
+                height="400"
+                className="h-full object-cover rounded-[10px]"
+              />
+              <div className={styles.builder_card_details}>
+                <CardTitle className={styles.CardTitle}>
+                  Tech Rational
+                </CardTitle>
+                <CardDescription>
+                  Technology, Information and Media • London
+                </CardDescription>
+              </div>
+            </Card>
+            <Card className={styles.builder_cards}>
+              <Image
+                src="/images/techr_logo.png"
+                alt="Image"
+                width="400"
+                height="400"
+                className="h-full object-cover rounded-[10px]"
+              />
+              <div className={styles.builder_card_details}>
+                <CardTitle className={styles.CardTitle}>
+                  Tech Rational
+                </CardTitle>
+                <CardDescription>
+                  Technology, Information and Media • London
+                </CardDescription>
+              </div>
+            </Card>
+            <Card className={styles.builder_cards}>
+              <Image
+                src="/images/techr_logo.png"
+                alt="Image"
+                width="400"
+                height="400"
+                className="h-full object-cover rounded-[10px]"
+              />
+              <div className={styles.builder_card_details}>
+                <CardTitle className={styles.CardTitle}>
+                  Tech Rational
+                </CardTitle>
+                <CardDescription>
+                  Technology, Information and Media • London
+                </CardDescription>
+              </div>
+            </Card>
+            <Card className={styles.builder_cards}>
+              <Image
+                src="/images/techr_logo.png"
+                alt="Image"
+                width="400"
+                height="400"
+                className="h-full object-cover rounded-[10px]"
+              />
+              <div className={styles.builder_card_details}>
+                <CardTitle className={styles.CardTitle}>
+                  Tech Rational
+                </CardTitle>
+                <CardDescription>
+                  Technology, Information and Media • London
+                </CardDescription>
+              </div>
+            </Card>
+            <Card className={styles.builder_cards}>
+              <Image
+                src="/images/techr_logo.png"
+                alt="Image"
+                width="400"
+                height="400"
+                className="h-full object-cover rounded-[10px]"
+              />
+              <div className={styles.builder_card_details}>
+                <CardTitle className={styles.CardTitle}>
+                  Tech Rational
+                </CardTitle>
+                <CardDescription>
+                  Technology, Information and Media • London
+                </CardDescription>
+              </div>
+            </Card>
+            <Card className={styles.builder_cards}>
+              <Image
+                src="/images/techr_logo.png"
+                alt="Image"
+                width="400"
+                height="400"
+                className="h-full object-cover rounded-[10px]"
+              />
+              <div className={styles.builder_card_details}>
+                <CardTitle className={styles.CardTitle}>
+                  Tech Rational
+                </CardTitle>
+                <CardDescription>
+                  Technology, Information and Media • London
+                </CardDescription>
+              </div>
+            </Card>
+            <Card className={styles.builder_cards}>
+              <Image
+                src="/images/techr_logo.png"
+                alt="Image"
+                width="400"
+                height="400"
+                className="h-full object-cover rounded-[10px]"
+              />
+              <div className={styles.builder_card_details}>
+                <CardTitle className={styles.CardTitle}>
+                  Tech Rational
+                </CardTitle>
+                <CardDescription>
+                  Technology, Information and Media • London
+                </CardDescription>
+              </div>
+            </Card>
+            <Card className={styles.builder_cards}>
+              <Image
+                src="/images/techr_logo.png"
+                alt="Image"
+                width="400"
+                height="400"
+                className="h-full object-cover rounded-[10px]"
+              />
+              <div className={styles.builder_card_details}>
+                <CardTitle className={styles.CardTitle}>
+                  Tech Rational
+                </CardTitle>
+                <CardDescription>
+                  Technology, Information and Media • London
+                </CardDescription>
+              </div>
+            </Card>
+          </div>
         </div>
-      </div>
-      {/* ////////////////////// */}
+        {/* ////////////////////// */}
       </Sheet>
     </>
   );
--- src/app/home/layout.jsx
+++ src/app/home/layout.jsx
@@ -149,7 +149,7 @@ export default function RootLayout({ children }) {
             <Tooltip>
               <TooltipTrigger asChild>
                 <Link
-                  href="home/builder"
+                  href="/home/builder"
                   className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
                 >
                   {/* <LineChart className="h-5 w-5" /> */}
